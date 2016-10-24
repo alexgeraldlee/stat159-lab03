64bd1f8d4eb3b94f67691026bd8a2c48fe28bdd0
Alexander Lee <tofu.da.beast.42@gmail.com>
2016-10-24 10:07:38 -0700

    Removed unnecessary text that was copied from example file (specifically regarding colors and a k-means output)

--- lab08/downloads/code/app.R
+++ lab08/downloads/code/app.R
@@ -1,9 +1,5 @@
 advertising <- read.csv("Advertising.csv")
 
-# 01-kmeans-app
-
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
 library(shiny)
 
